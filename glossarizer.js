cd5a3e570636f58d8c54b16e3ddcf74c046d01ee
Yashar Ghaemi <yashar.ghaemi@antecedent.us>
2026-06-19 13:13:09 -0400

    Update glossary script to increase font size for tooltips, captions, definitions, sources, and links, enhancing readability and consistency.

--- glossarizer.js
+++ glossarizer.js
@@ -179,12 +179,12 @@
             style = document.createElement('style');
             style.id = 'gt-styles';
             style.textContent = [
-                '.gt-tooltip{max-width:280px;font-size:8px;line-height:1;color:#000; font-size:0.8rem; line-height:1; border-radius:0.5rem;}',
+                '.gt-tooltip{max-width:280px;line-height:1;color:#000; font-size:0.9rem; line-height:1; border-radius:0.5rem;}',
                 '.gt-img-wrap{margin-bottom:8px;display:flex;justify-content:center;flex-direction:column;}',
-                '.gt-caption{margin:4px 0 0;font-size:0.8rem!important,font-weight:normal!important;color:#4f4545;text-align:center;}',
-                '.gt-definition{margin:0 0 6px; font-size:0.8rem!important; line-height:1.5!important; font-weight:normal!important;}',
-                '.gt-source{margin:4px 0;font-size:11px;color:#aaa;}',
-                '.gt-link{display:inline-block;margin-top:4px;font-size:12px;color:#4a90e2;text-decoration:none;}',
+                '.gt-caption{margin:4px 0 0;font-size:0.9rem!important,font-weight:normal!important;color:#4f4545;text-align:center;}',
+                '.gt-definition{margin:0 0 6px; font-size:0.9rem!important; line-height:1.5!important; font-weight:normal!important;}',
+                '.gt-source{margin:4px 0;font-size:0.9rem;color:#aaa;}',
+                '.gt-link{display:inline-block;margin-top:4px;font-size:0.9rem;color:#4a90e2;text-decoration:none;}',
                 '.gt-link:hover{text-decoration:underline;}',
                 '.tippy-box[data-theme~="light"]{background-color:#ffffff; border-radius:0.2rem;  border:1px solid #4a90e2;}',
                 '.tippy-box[data-theme~="light"] .tippy-content{padding:10px 14px;font-size:0.8rem; line-height:1;}'
