fc9a171eda8e6c43f50b152037198d6420b5c29e
Yashar Ghaemi <yashar.ghaemi@antecedent.us>
2026-09-17 15:29:45 -0400

    Update API_HOST for development environment
    - Changed API_HOST to a new URL for improved testing and development.
    - Updated corresponding minified files to reflect this change.

--- dist/glossarizer.js
+++ dist/glossarizer.js
@@ -70,7 +70,7 @@
   }
 
   // src/config.ts
-  var API_HOST = "https://undertaken-creations-keith-victorian.trycloudflare.com";
+  var API_HOST = "https://option-shelf-animals-destiny.trycloudflare.com";
   var CACHE_TTL = 60 * 60 * 1e3;
   var LICENSE_MAP = {
     arr: "All Rights Reserved",
