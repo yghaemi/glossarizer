747411387499d98611bfc2aa9a1f9b092a1cbee0
Yashar Ghaemi <yashar.ghaemi@antecedent.us>
2026-09-24 18:17:16 -0400

    Update API_HOST for production environment
    - Changed API_HOST to a new URL for production use, enhancing the application's connectivity.
    - Updated corresponding minified files to reflect this change.

--- dist/glossarizer.js
+++ dist/glossarizer.js
@@ -70,7 +70,7 @@
   }
 
   // src/config.ts
-  var API_HOST = "https://synthetic-lip-writes-thee.trycloudflare.com";
+  var API_HOST = "https://commons.libretexts.org";
   var CACHE_TTL = 60 * 60 * 1e3;
   var LICENSE_MAP = {
     arr: "All Rights Reserved",
